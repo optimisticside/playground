44a9be94a9a5b42b9b222ebcba9bc9c51de19b5e
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-12 13:03:23 -0800

    Added better comments

--- forkbomb/source.asm
+++ forkbomb/source.asm
@@ -2,6 +2,6 @@ section.text:
   global _start
 
 _start:
-  mov eax, 2  ; syscall code for fork
-  int 0x80    ; interrupt code for syscall
+  mov eax, 2  ; syscall number for fork
+  int 0x80    ; issue a syscall and call kernel
   jmp _start  ; jump back to start and repeat
