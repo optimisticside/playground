2dcc0761ebc3cc7dd1b108a59e92f34a646afbc7
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-13 09:43:57 -0800

    Added universe generator

--- gameoflife/source.c
+++ gameoflife/source.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 /* constant defenition */
 #define CYCLE_TIME  1000    /* amount of ms for each cycle to take place */
@@ -14,15 +16,23 @@ struct cell {
 struct univ {
     struct cell *cells;     /* cells in the universe */
     uint32_t cell_count;    /* amount of cells */
+    uint8_t updated;        /* whether the last evolution updated the world */
 
     uint32_t column_count;  /* amount of rows */
     uint32_t row_count;     /* amount of columns */
 };
 
+/* utility function to create a random number
+   the rand function is just garbling numbers, but it doesn't matter */
+uint32_t rand_int(uint32_t a, uint32_t b) {
+    return (rand() % (b - a)) + a;
+}
+
 /* gets a cell from its coordinates */
 struct cell *get_cell(struct univ *world, uint32_t x, uint32_t y) {
     /* get the starting position of the row and the cell in the row
-       (row_size * row_pos) + column_pos */
+       (row_size * row_pos) + column_pos
+       note that the cells are arranged in a MATRIX, meaning the first row is the top one */
     return world->cells[world->column_count * x + y];
 }
 
@@ -84,5 +94,48 @@ void evolve(struct univ *world) {
                 cell->alive = 1;
             }
         }
+    }
+}
+
+/* creates a universe */
+struct univ *create(uint32_t size_x, uint32_t size_y, uint32_t alive) {
+    /* allocate universe */
+    struct univ *world = (struct univ *)malloc(sizeof(struct univ));
+
+    /* initailize members */
+    world->cell_count = size_x * size_y;
+    world->column_count = size_x;
+    world->row_count = size_y;
+    world->cells = (struct cell *)calloc(world->cell_count, sizeof(struct cell));
+
+    /* initialize cells */
+    for (uint32_t i = 0; i < world->cell_count; i++) {
+        /* get cell */
+        struct cell *cell = &world->cells[i];
+
+        /* determine coordinates */
+        uint32_t y = i / world->column_count;
+        uint32_t x = i % world->column_count;
+
+        /* initailize members */
+        cell->x = x;
+        cell->y = y;
+    }
+
+    /* make random cells alive */
+    for (uint32_t i = 0; i < alive && i < world->cell_count; i++) {
+        /* store the cell to make alive here */
+        struct cell *cell;
+
+        /* find a cell */
+        do {
+            /* find random cell */
+            cell = world->cells[rand_int(0, world->cell_count-1)];
+
+        /* keep repeating until alive cell found */
+        } while (cell->alive);
+
+        /* make cell alive */
+        cell->alive = 1;
     }
 }
